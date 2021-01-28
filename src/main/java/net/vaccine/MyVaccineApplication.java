9be78d5958c2b4d71ea9babb8525f5b8916c14f9
DESKTOP-4IDT16J\lth11 <lth1178@gmail.com>
2021-01-28 18:18:46 +0900

    no message

---
+++ src/main/java/net/vaccine/MyVaccineApplication.java
@@ -0,0 +1,13 @@
+package net.vaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVaccineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVaccineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/vaccine/MyVaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVaccineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
